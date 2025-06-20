e07722b13290164977e21b9f8fbfb0adfa52d414
a1937486 <a1937486@adelaide.edu.au>
2025-06-20 03:41:32 +0000

    [automatic commit] 2025-06-20 03:41:32 UTC

--- part2/app.js
+++ part2/app.js
@@ -15,5 +15,8 @@ const userRoutes = require('./routes/userRoutes');
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
+const walkRoutes = require('./routes/walkRoutes');
+app.use('/api', walkRoutes);  
+
 // Export the app instead of listening here
 module.exports = app;
