c70862296067bbc69068b9aaa886773b78d9b124
a1937486 <a1937486@adelaide.edu.au>
2025-06-20 03:23:10 +0000

    [automatic commit] 2025-06-20 03:23:09 UTC

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
 
+
 // GET all walk requests (for walkers to view)
 router.get('/', async (req, res) => {
   try {
