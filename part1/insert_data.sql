8b4ac60e9260c188e3b1b71fd3e30fa9b2002e91
a1937486 <a1937486@adelaide.edu.au>
2025-06-21 05:13:41 +0000

    [automatic commit] 2025-06-21 05:13:41 UTC

--- part1/insert_data.sql
+++ part1/insert_data.sql
@@ -5,8 +5,8 @@ INSERT INTO Users (username, email, password_hash, role) VALUES
 ('alice123', 'alice@example.com', 'hashed123', 'owner'),
 ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
 ('carol123', 'carol@example.com', 'hashed789', 'owner'),
-('Paulo', '@example.com', 'hashedabc', 'owner'),
-('evewalker', 'eve@example.com', 'hasheddef', 'walker');
+('Paulo', 'paulo@example.com', 'hashed333', 'owner'),
+('maison', 'eve@example.com', 'hasheddef', 'walker');
 
 
 
